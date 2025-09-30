e2015e13fd303f8fcf3d1ab8751d3cb58f4300a1
Dapaa1105 <dafanr2307@gmail.com>
2025-09-30 14:37:09 +0700

    Modifikasi Percobaan 1

--- Jobsheet5/ifCetakKRS05.java
+++ Jobsheet5/ifCetakKRS05.java
@@ -3,18 +3,22 @@ package Jobsheet5;
 import java.util.Scanner;
 
 public class ifCetakKRS05 {
-   public ifCetakKRS05() {
-   }
-
+   
    public static void main(String[] var0) {
       Scanner sc = new Scanner(System.in);
+
       System.out.println("--- Cetak KRS SIAKAD ---");
       System.out.println("Apakah UKT sudah lunas? (true/false): ");
       boolean uktLunas = sc.nextBoolean();
+
       if (uktLunas) {
          System.out.println("Pembayaran UKT terverifikasi");
          System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
 
+      }
+      else{
+         System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+         
       }
       sc.close();
 }
