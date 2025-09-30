da4bd965b85c813fc4e1f161d68809a86bcdec82
Dapaa1105 <dafanr2307@gmail.com>
2025-09-30 17:44:19 +0700

    Tugas 3

---
+++ Jobsheet5/sistemPerpustakaan.java
@@ -0,0 +1,26 @@
+package Jobsheet5;
+
+import java.util.Scanner;
+
+public class sistemPerpustakaan {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String kehadiran;
+
+        System.out.println("=== Sistem Kehadiran Mahasiswa ===");
+        System.out.print("Apakah kehadiran menggunakan kartu mahasiswa? (yes/no): ");
+        kehadiran = input.nextLine();
+        
+        if (kehadiran.equalsIgnoreCase("yes")) {
+            System.out.println("Output Masuk Kampus");
+        } else {
+            System.out.println("Silahkan Register Online terlebih dahulu.");
+            System.out.println("Setelah Register Online, baru bisa masuk kampus. ");
+
+        } System.out.println("=== Selesai ===");
+        input.close();
+        
+    }
+    
+}
