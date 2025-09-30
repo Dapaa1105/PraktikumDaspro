0ae582fcb6aac1b85426405f72c98839f80b5d6d
Dapaa1105 <dafanr2307@gmail.com>
2025-09-30 15:42:01 +0700

    Tugas 1

--- Jobsheet5/ifCetakKRS05.java
+++ Jobsheet5/ifCetakKRS05.java
@@ -11,14 +11,19 @@ public class ifCetakKRS05 {
       System.out.println("Apakah UKT sudah lunas? (true/false): ");
       boolean uktLunas = sc.nextBoolean();
 
+      String hasil = (uktLunas) 
+    ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA"
+    : "Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu";
+
+    System.out.println(hasil);
+
       if (uktLunas) {
          System.out.println("Pembayaran UKT terverifikasi");
          System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-
       }
       else{
          System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
-         
+
       }
       sc.close();
 }
