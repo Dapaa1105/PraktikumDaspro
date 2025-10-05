9b6443c0080abc391d8b84e000813fa37eb9721a
Dapaa1105 <dafanr2307@gmail.com>
2025-10-05 19:25:15 +0700

    Tugas 3

---
+++ Jobsheet5/aksesWIFIKampus.java
@@ -0,0 +1,34 @@
+package Jobsheet5;
+
+import java.util.Scanner;
+
+public class aksesWIFIKampus {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String jenisPengguna;
+        int jmlSks = 0;
+
+        System.out.println("=== Sistem Akses Wifi Kampus ===");
+        System.out.print("Masukkan jenis pengguna (dosen/mahasiswa): ");
+        jenisPengguna = input.nextLine();
+
+        if (jenisPengguna.equalsIgnoreCase("dosen")) {
+            System.out.println("Akses Wifi diberikan (dosen). ");
+        } else if (jenisPengguna.equalsIgnoreCase("mahasiswa")) {
+            System.out.println("Masukkan jumlah SKS yang diambil: ");
+            jmlSks = input.nextInt();
+
+            if (jmlSks >= 12) {
+                System.out.println("Akses Wifi diberikan (Mahasiswa Aktif)");
+            } else {
+                System.out.println("Akses Wifi ditolak (Jumlah SKS < 12)");
+            }
+        } else {
+            System.out.println("Jenis pengguna tidak dikenali");
+        }
+        input.close();
+        System.out.println("=== Proses Selesai ===");
+    }
+    
+}
