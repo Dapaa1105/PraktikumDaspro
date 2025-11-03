4b79be54386338cf587c0f49567bffcf62279a79
Dapaa1105 <dafanr2307@gmail.com>
2025-11-03 20:19:17 +0700

    Modifikasi Percobaan 3

--- Jobsheet8/triangle05.java
+++ Jobsheet8/triangle05.java
@@ -6,13 +6,14 @@ public class triangle05 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan nilai n = ");
         int n = sc.nextInt();
-        int i = 0;
+        int i = 1;
         while (i<= n) {
             int j = 0;
             while (j < i) {
                 System.out.print("*");
                 j++;
             }
+            System.out.println();
             i++;
     }
     sc.close();
