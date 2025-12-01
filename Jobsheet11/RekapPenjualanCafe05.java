b2f39e0c454ef5df24773d5e0ceeba1cc7f966ff
Dapaa1105 <dafanr2307@gmail.com>
2025-12-01 20:03:44 +0700

    Modifikasi Tugas 3

--- Jobsheet11/RekapPenjualanCafe05.java
+++ Jobsheet11/RekapPenjualanCafe05.java
@@ -1,43 +1,43 @@
 package Jobsheet11;
 
-public class RekapPenjualanCafe05 {
-    static String[] menu = {"Kopi", "Teh", "Es Kelapa Muda", "Roti Bakar", "Gorengan"};
+import java.util.Scanner;
 
-    static int[][] inputData() {
-        int[][] data = {
-            {20, 20, 25, 20, 10, 60, 10},
-            {30, 80, 40, 10, 15, 20, 25},
-            {5, 9, 20, 25, 10, 5, 45},
-            {50, 8, 17, 18, 10, 30, 6},
-            {15, 10, 16, 15, 10, 10, 55}
-        };
-        return data;
+public class RekapPenjualanCafe05 {
+
+    static void inputData(int[][] data, String[] menu) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println("Input penjualan untuk menu: " + menu[i]);
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                data[i][j] = sc.nextInt();
+            }
+        }
     }
 
-    static void tampilkanData(int[][] data) {
-        System.out.println("Data Penjualan Kafe (7 Hari)");
+    static void tampilkanData(int[][] data, String[] menu) {
         System.out.print("Menu        ");
-        for (int i = 1; i <= 7; i++) {
+        for (int i = 1; i <= data[0].length; i++) {
             System.out.print("Hari " + i + "  ");
         }
         System.out.println();
 
         for (int i = 0; i < menu.length; i++) {
             System.out.print(menu[i] + "   ");
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < data[i].length; j++) {
                 System.out.print(data[i][j] + "       ");
             }
             System.out.println();
         }
     }
 
-    static void penjualanTertinggi(int[][] data) {
+    static void penjualanTertinggi(int[][] data, String[] menu) {
         int maxTotal = 0;
         String menuTertinggi = "";
 
         for (int i = 0; i < menu.length; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < data[i].length; j++) {
                 total += data[i][j];
             }
             if (total > maxTotal) {
@@ -49,22 +49,38 @@ public class RekapPenjualanCafe05 {
         System.out.println("Menu dengan total penjualan tertinggi: " + menuTertinggi + " (" + maxTotal + ")");
     }
 
-    static void rataRataPenjualan(int[][] data) {
-        System.out.println("Rata-rata penjualan setiap menu");
+    static void rataRataPenjualan(int[][] data, String[] menu) {
         for (int i = 0; i < menu.length; i++) {
             int total = 0;
-            for (int j = 0; j < 7; j++) {
+            for (int j = 0; j < data[i].length; j++) {
                 total += data[i][j];
             }
-            double rata = total / 7.0;
+            double rata = total / (double) data[i].length;
             System.out.println(menu[i] + ": " + rata);
         }
     }
 
     public static void main(String[] args) {
-        int[][] data = inputData();
-        tampilkanData(data);
-        penjualanTertinggi(data);
-        rataRataPenjualan(data);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah menu: ");
+        int jumlahMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah hari: ");
+        int jumlahHari = sc.nextInt();
+
+        String[] menu = new String[jumlahMenu];
+        sc.nextLine();
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Masukkan nama menu ke-" + (i + 1) + ": ");
+            menu[i] = sc.nextLine();
+        }
+
+        int[][] data = new int[jumlahMenu][jumlahHari];
+
+        inputData(data, menu);
+        tampilkanData(data, menu);
+        penjualanTertinggi(data, menu);
+        rataRataPenjualan(data, menu);
     }
 }
