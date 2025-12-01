ed696ad26329f143d99cec230c5435df9d6a158c
Dapaa1105 <dafanr2307@gmail.com>
2025-12-01 19:07:49 +0700

    Percobaan 3

--- Jobsheet11/kafe05.java
+++ Jobsheet11/kafe05.java
@@ -1,6 +1,9 @@
 package Jobsheet11;
 
+import java.util.Scanner;
+
 public class kafe05 {
+
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
@@ -8,7 +11,6 @@ public class kafe05 {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
 
-        // Promo
         if(kodePromo.equals("DISKON50")) {
             System.out.println("Kode promo valid! Anda mendapatkan diskon 50%!");
         } else if(kodePromo.equals("DISKON30")) {
@@ -17,18 +19,36 @@ public class kafe05 {
             System.out.println("Kode promo invalid.");
         }
 
-        System.out.println("==== MENU RESTO KAFE ====");
+        System.out.println("\n==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
         System.out.println("4. Teh Tarik - Rp 12,000");
         System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie bakar - Rp 18,000");
+        System.out.println("6. Mie Bakar - Rp 18,000");
         System.out.println("===========================");
-        System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    }
+
+    public static int hitungTotalHargaNoPresensi(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
     }
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         Menu("Andi", true, "DISKON50");
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalBayar = hitungTotalHargaNoPresensi(pilihanMenu, banyakItem);
+
+        System.out.println("\nTotal harga untuk pesanan Anda: Rp " + totalBayar);
     }
 }
